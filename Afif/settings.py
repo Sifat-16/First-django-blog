cf4d4674eb00129068ee3f0b6f9da424ba595ddf
Sifat-16 <59054820+Sifat-16@users.noreply.github.com>
2020-06-30 00:42:02 +0600

    Securing

--- Afif/settings.py
+++ Afif/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$i9@xn_-n)ha)d(-90d7mud5&bc1il0)%^rg-3h8me*pmlo-q!'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
